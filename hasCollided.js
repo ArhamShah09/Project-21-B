8a6e16fc0e5c04f282b96f7eab95a45ba9d40acf
ArhamShah09 <65812990+ArhamShah09@users.noreply.github.com>
2020-05-31 16:41:50 +0530

    Add files via upload

---
+++ hasCollided.js
@@ -0,0 +1,8 @@
+function hasCollided(lbullet,lwall) {
+    bulletRightEdge = lbullet.x + bullet.width;
+    wallLeftEdge = lwall.x
+    if(bulletRightEdge >= wallLeftEdge) {
+        return true;
+    }
+    return false;
+}
